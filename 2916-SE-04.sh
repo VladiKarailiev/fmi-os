31308047ea479db5b862ef398e83bedaf8092973
Vladi Karailiev <vladikarailiev2004@gmail.com>
2025-04-06 19:58:59 +0300

    Solved 2916-SE-04.sh

---
+++ 2916-SE-04.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mkdir a
+mkdir b
+mkdir c
+
+if [[ "$#" -ne 2 ]];
+then
+    echo "Not 2 params"
+    exit 1
+fi
+
+if [[ $1 -gt $2 ]];
+then
+    echo "First parameter can't be less than the second"
+    exit 1
+fi
+
+for file in *; do
+    if [[ -f $file ]]; then
+        lines=$(wc -l $file | cut -d ' ' -f 1)
+        echo "$file has $lines lines and is moved to: "
+        if [[ $lines -lt $1 ]]; then
+            mv $file "/a"
+            echo "a"
+        else
+        if [[ $lines -lt $2 ]]; then
+            mv $file "/b"
+            echo "b"
+        else
+            mv $file "/c"
+            echo "c"
+        fi
+        fi
+        fi
+done
