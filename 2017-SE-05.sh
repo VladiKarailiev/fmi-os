e42056d32d4e5657f423157f02d1eec60ff85e41
Vladi Karailiev <vladikarailiev2004@gmail.com>
2025-04-07 21:11:26 +0300

    Create 2017-SE-05.sh

---
+++ 2017-SE-05.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+dir=$1
+arch=$2
+
+bestFile=""
+bestX=0
+bestY=0
+bestZ=0
+
+while read -r file
+do
+    version=$(echo $file | cut -d "-" -f 2 )
+    currArch=$(echo $file | cut -d "-" -f 3)
+
+    x=$(echo $version | cut -d "." -f 1)
+    y=$(echo $version | cut -d "." -f 2)
+    z=$(echo $version | cut -d "." -f 3)
+
+
+    if [[ $currArch != $arch ]]; then
+        continue
+    fi
+
+    echo "currently best: $bestX.$bestY.$bestZ and the  current is $x.$y.$z"
+
+    if [[ $x -gt $bestX ]]; then
+        bestFile=$file
+        bestX=$x
+        bestY=$y
+        bestZ=$z
+
+    elif [[ $y -gt $bestY ]]; then
+        bestFile=$file
+        bestX=$x
+        bestY=$y
+        bestZ=$z
+    elif [[ $z -gt $bestZ ]]; then
+        bestFile=$file
+        bestX=$x
+        bestY=$y
+        bestZ=$z
+    fi
+
+done < <(find $dir -maxdepth 1 -regex .*/vmlinuz-\[0-9\]*\.\[0-9\]*\.\[0-9\]*-.*)
+
+echo $bestFile
