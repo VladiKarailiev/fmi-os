0c2d3848ec981d6a84b64907114b36757d73dbb4
Vladi Karailiev <vladikarailiev2004@gmail.com>
2025-04-07 20:31:00 +0300

    Create 2017-SE-03.sh

---
+++ 2017-SE-03.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+lastUid=""
+count=0
+total=0
+userBestSize=0
+userBestPID=""
+
+while read -r line;
+do
+    currUid=$( echo $line | cut -d " " -f 1)
+    currPss=$( echo $line | cut -d " " -f 3)
+    currPID=$( echo $line | cut -d " " -f 2)
+    if [[ $currUid == $lastUid ]]; then
+        count=$(( count + "1" ))
+        total=$(( total + currPss ))
+        if [[ $currPss -gt $userBestSize ]]; then
+            userBestSize=$currPss
+            userBestPID=$currPID
+        fi
+    else
+        if [[ $count -eq 0 ]]; then
+            continue
+        fi
+
+        echo "$lastUid has $count processes with $total memory"
+
+        if [[ $(( total / count )) -lt $userBestSize ]]; then
+            echo "Process $userBestPID should be terminated."
+        fi
+        lastUid=$currUid
+        total=$currPss
+        count=1
+        userBestSize=$currPss
+        userBestPID=$currPID
+
+    fi
+
+done < <(ps -e -o uid,pid,rss | sort )
