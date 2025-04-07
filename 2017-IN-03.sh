ecbdd2796dd662bba30a34bfc8d3a325ec4d5185
Vladi Karailiev <vladikarailiev2004@gmail.com>
2025-04-07 19:11:01 +0300

    Create 2017-IN-03.sh

---
+++ 2017-IN-03.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+bestFile=""
+bestTime=0
+
+while read -r line;
+do
+    dir=$(echo $line | cut -d ":" -f 6)
+    #echo $dir
+
+    for i in "$dir"/*; do
+
+        if [[ ! -f $i ]]; then
+            continue
+        fi
+        echo "checking $i"
+        currTime=$(stat $i -c %Y 2>"/dev/null")
+
+        if [[ $bestTime -lt $currTime ]]; then
+            bestTime=$currTime
+            bestFile=$i
+        fi
+    done
+done < <( cat /etc/passwd  )
+
+echo "$bestFile modified at: $bestTime"
