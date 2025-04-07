13deab413a670cc2fb79b681e3065dc35a3a36b9
Vladi Karailiev <vladikarailiev2004@gmail.com>
2025-04-07 20:09:28 +0300

    Create 2017-SE-02.sh

---
+++ 2017-SE-02.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $# -ne "3" ]]; then
+    exit 1
+fi
+
+src=$1
+dst=$2
+str=$3
+
+if [[ ! -d $src ]] || [[ ! -d $dst ]]
+    then echo "SRC and DST need to be directories"
+elif [[ ! -z $(ls -A $dst) ]]
+    then echo "DST needs to be empty!"
+fi
+
+find $1 -regex ".*${str}.*" -exec mv -i {} $dst \;
