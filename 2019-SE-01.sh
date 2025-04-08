73003bfb38f2dc1ca3c2da72bb170a3c13250cac
Vladi Karailiev <vladikarailiev2004@gmail.com>
2025-04-08 11:42:11 +0300

    Create 2019-SE-01.sh

---
+++ 2019-SE-01.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+input=$(mktemp)
+
+max=0
+maxSum=0
+
+digits()
+{
+    num=$1
+    sum=0
+
+    while [[ $num -gt 0 ]]
+    do
+    digit=$(( num % 10))
+    sum=$(( sum + digit ))
+    num=$(( num / 10 ))
+
+    done
+
+    echo $sum
+}
+
+while read -r line; do
+    if [[ $line == "exit" ]]; then
+        break
+    fi
+
+    positive=$(echo $line | grep -e "^[0-9]*$")
+    negative=$(echo $line | grep -e "^-[0-9]*$")
+    echo $line >> $input
+
+    if [[ -z $positive ]] && [[ -z $negative ]]; then
+        echo "$line ne e chislo"
+        continue
+    elif [[ ! -z $negative ]]; then
+        line=$(( line * -1 ))
+    fi
+
+    if [[ $(digits $line) -gt $maxSum ]]; then
+        maxSum=$(digits $line)
+    fi
+
+    if [[ $line -gt $max ]]; then
+        max=$line
+    fi
+
+    echo $(digits $line)
+
+done
+
+echo "max digits sum: $maxSum"
+
+grep -e "^-$max$" $input | uniq
+grep -e "^$max$" $input | uniq
